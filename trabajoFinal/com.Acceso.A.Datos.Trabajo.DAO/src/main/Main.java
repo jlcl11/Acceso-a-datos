9eda33d9b85b1b23fcdd7c0bf9e74f6986c5ca54
jlcl11 <92518378+jlcl11@users.noreply.github.com>
2022-12-02 15:00:22 +0100

    prueba de funcion buscarPlantilla
    Es una prueba,sigue sin funcionar

--- trabajoFinal/com.Acceso.A.Datos.Trabajo.DAO/src/main/Main.java
+++ trabajoFinal/com.Acceso.A.Datos.Trabajo.DAO/src/main/Main.java
@@ -8,20 +8,25 @@ import pojo.Jugador;
 public class Main {
 
 	public static void main(String[] args) {
-	
-		EquipoDao ed=new EquipoDao();
-		Equipo lakers= new Equipo("Clippers", "Un random", "Rob Pelinka", "Steve Balmer", false);
-		
-		//System.out.println(ed.buscarPorId(3));
-		//ed.insertar(lakers);
-		//ed.modificar(lakers);
-		
-		Jugador lbj=new Jugador(2,"jojo", "R", null, (byte) 0, 0, null, 3);
-		JugadorDao jd=new JugadorDao();
-		
-		//jd.insertar(lbj);
-		
-		jd.borrar(lbj);;
+
+		EquipoDao ed = new EquipoDao();
+		Equipo lakers = new Equipo("Clippers", "Un random", "Rob Pelinka", "Steve Balmer", false);
+		ed.insertar(lakers);
+
+		Equipo aux = ed.buscarPorId(2);
+		System.out.println(ed.buscarTodos());
+		System.out.println(ed.buscarPlantilla(aux));
+
+		// System.out.println(ed.buscarPorId(3));
+		// ed.insertar(lakers);
+		// ed.modificar(lakers);
+
+		Jugador lbj = new Jugador(2, "jojo", "R", null, (byte) 0, 0, null, 1);
+		JugadorDao jd = new JugadorDao();
+
+		jd.insertar(lbj);
+
+		// System.out.println(jd.buscarTodos());
 	}
 
 }
